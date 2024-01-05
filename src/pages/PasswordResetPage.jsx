b34a5b9cb926c78e688d17abf9a537ecd4f34880
Dun Huang <hd200410190@gmail.com>
2024-01-05 17:15:47 -0500

    Password reset page (temp)

--- src/pages/PasswordResetPage.jsx
+++ src/pages/PasswordResetPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import AppTitle from '../components/AppTitle';
+import { Container, Button, Row, Col, Alert } from 'react-bootstrap';
 
 const PasswordResetPage = () => {
   return (
-    <div>
-      <h2>Welcome to the Password Reset Page!</h2>
-    </div>
+    <Container>
+      <AppTitle />
+        <Row className="mt-5 login-form-box">
+          <Col className="auth-form">
+          <Alert variant='info'>
+            If you forgot your password, please email the system admin at jasonchenatlanta@gmail.com to retrieve it.
+          </Alert>
+          <Button variant="secondary" href='/login' className="auth-form-button">
+            Back To Login
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
